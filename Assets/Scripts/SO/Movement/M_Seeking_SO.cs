fb6b1268226ebf837ec334ffa95552d503690e89
parsa <48556656+parsarafiee@users.noreply.github.com>
2022-04-22 11:03:18 -0400

    ArcherTower
    - Model is done (with Blender)
    - ArcherTower Prefab is done
    - Added in the parsa scene to Test

--- Assets/Scripts/SO/Movement/M_Seeking_SO.cs
+++ Assets/Scripts/SO/Movement/M_Seeking_SO.cs
@@ -10,18 +10,25 @@ using General;
 
 public class M_Seeking_SO : Movement_SO
 {
+
+    private Enemy enemy;
     public override void Refresh()
     {
 
-        if (!target)
+        if (target == null || enemy.alive == false)
         {
-            IPoolable p = gameobject.GetComponent<IPoolable>();
-            ObjectPool.Instance.Pool(type, p);
+            ObjectPool.Instance.Pool(type, ipoolable);
             return;
         }
         rb.velocity = (target.position - gameobject.transform.position).normalized * initialSpeed;
         gameobject.transform.forward = rb.velocity.normalized;
 
+
+    }
+    public override void Init(GameObject _gameObject, ProjectileType _type, Transform _target, float speed, Vector3 _projectileInitialDIrection)
+    {
+        base.Init(_gameObject, _type, _target, speed, _projectileInitialDIrection);
+        enemy = _target.gameObject.GetComponent<Enemy>();
     }
 
 }
--- Assets/Scripts/SO/Movement/Movement_SO.cs
+++ Assets/Scripts/SO/Movement/Movement_SO.cs
@@ -1,25 +1,28 @@
 using UnityEngine;
 using Units.Types;
+using General;
+
 public class Movement_SO : ScriptableObject
 {
-    protected GameObject gameobject;
-    
-    public Transform target;
-    public Rigidbody rb;
-    public Vector3 projectileInitialDIrection;
-    public float initialSpeed;
     public ProjectileType type;
-    public Enemy enemy;
+    [HideInInspector]protected GameObject gameobject;
+    [HideInInspector]public IPoolable ipoolable;
+    [HideInInspector]public Transform target;
+    [HideInInspector]public Rigidbody rb;
+    [HideInInspector]public Vector3 projectileInitialDIrection;
+    [HideInInspector]public float initialSpeed;
+    
     public virtual void FixedRefresh()
     {
     }
 
 
-    public virtual void Init(GameObject _gameObject, ProjectileType _type, Transform _targetTransform,float speed,Vector3 _projectileInitialDIrection)
+    public virtual void Init(GameObject _gameObject, ProjectileType _type, Transform _target,float speed,Vector3 _projectileInitialDIrection)
     {
         gameobject = _gameObject;
+        ipoolable = gameobject.GetComponent<IPoolable>();
         type = _type;
-        target = _targetTransform;
+        target = _target;
         rb = gameobject.GetComponent<Rigidbody>();
         initialSpeed = speed;
         projectileInitialDIrection = _projectileInitialDIrection;
