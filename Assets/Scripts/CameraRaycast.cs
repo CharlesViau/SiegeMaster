35068fba52465e40af6782fac3f75e85c326f879
parsa <48556656+parsarafiee@users.noreply.github.com>
2022-05-05 11:23:18 -0400

    Camera Raycast for player
    made a function in camera to send position of raycastHit  for player to calculate the math for abilities,

--- Assets/Scripts/CameraRaycast.cs
+++ Assets/Scripts/CameraRaycast.cs
@@ -2,23 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraRatcast : MonoBehaviour
+public class CameraRaycast : MonoBehaviour
 {
-  
-    private RaycastHit hit;
-    [HideInInspector]
-    public Vector3 hitPoint;
-    public float maxDistanceForRay;
-    void FixedUpdate()
+    public Vector3 RayCast(float maxDistanceForRay)
     {
+        RaycastHit hit;
+        Vector3 hitPoint;
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
 
         if (Physics.Raycast(transform.position, fwd, out hit, maxDistanceForRay))
-            hitPoint =hit.point;
+        {
+            hitPoint = hit.point;
+            // Debug.Log(hitPoint);
+        }
         else
         {
             // if player look at sky we send the maxDistancePosition 
             hitPoint = transform.forward * maxDistanceForRay;
         }
+        return hitPoint;
     }
 }
